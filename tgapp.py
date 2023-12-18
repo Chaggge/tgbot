fcffd15363661c42cd12d9c9c2fd2b748bf57664
ilya <djimminetron@mail.ru>
2023-12-18 20:00:58 +0700

    weather

--- tgapp.py
+++ tgapp.py
@@ -1,8 +1,11 @@
 from flask import Flask, request
 import telebot
 import os
+import requests
+from telebot import types
 
 TOKEN_TG = os.environ.get("TOKEN_TG")
+WEATHERSTACK_API_KEY = os.environ.get('WEATHERSTACK_API_KEY')
 
 app = Flask(__name__)
 
@@ -14,14 +17,39 @@ bot = telebot.TeleBot(TOKEN_TG)
 # Установите URL для вебхука
 WEBHOOK_URL = 'https://echotg.onrender.com/' + TOKEN_TG
 
+
 # Установите вебхук
 bot.remove_webhook()
 bot.set_webhook(url=WEBHOOK_URL)
 
+def get_weather():
+    # Формируем URL для запроса погоды в Новосибирске
+    weatherstack_url = f'http://api.weatherstack.com/current?access_key={WEATHERSTACK_API_KEY}&query=Novosibirsk'
+    
+    # Отправляем GET-запрос к API
+    response = requests.get(weatherstack_url)
+    
+    # Проверяем успешность запроса
+    if response.status_code == 200:
+        data = response.json()
+        temperature = data['current']['temperature']
+        description = data['current']['weather_descriptions'][0]
+        return f'Текущая температура в Новосибирске: {temperature}°C\n{description}'
+    else:
+        return 'Не удалось получить информацию о погоде'
+
 # Обработчик команды /start
 @bot.message_handler(commands=['start'])
 def handle_start(message):
-    bot.send_message(message.chat.id, "Hello! I am your bot.")
+    markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1=types.KeyboardButton("/weather")
+    markup.add(item1)
+    bot.reply_to(message, "Привет я - эхо бот и не только?", reply_markup=markup)
+
+@bot.message_handler(commands=['weather'])
+def handle_weather(message):
+    weather_info = get_weather()
+    bot.send_message(message.chat.id, weather_info)
 
 # Обработчик текстовых сообщений
 @bot.message_handler(func=lambda message: True, content_types=['text'])
